e4665fe5b4a21a7df7dc46124b45c0648f28297a
Mahnaz shahin <farhangizhabibi721@gmail.com>
2024-11-06 13:09:42 -0800

    add image

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -24,7 +24,7 @@ const About = () => {
             </Link>
           </div>
           <div className="banner">
-            <img src="about.png" alt="about" />
+            <img src= "https://images.unsplash.com/photo-1513104890138-7c749659a591?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="about" />
           </div>
         </div>
       </section>
