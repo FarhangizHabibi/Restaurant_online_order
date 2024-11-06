beead51e605f87dc29f2c2b830ed05fcb647c44c
Mahnaz shahin <farhangizhabibi721@gmail.com>
2024-11-06 13:14:31 -0800

    add image

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -12,7 +12,7 @@ const HeroSection = () => {
           </div>
           <div className="combined_boxes">
             <div className="imageBox">
-              <img src="./coffeHero.jpg" alt="hero" />
+              <img src="https://images.unsplash.com/photo-1589187151053-5ec8818e661b?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.png" alt="hero" />
             </div>
             <div className="textAndLogo">
               <div className="textWithSvg">
