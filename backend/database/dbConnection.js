7f6716859abca7d337664f5a84f00a862307ffe6
FarhangizHabibi <150654811+FarhangizHabibi@users.noreply.github.com>
2024-11-10 10:48:39 +0430

    Update dbConnection.js

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const dbConnection = () => {
   mongoose
-    .connect("mongodb+srv://hamoon:123231213@cluster0.peugdn2.mongodb.net/wasteswift" )
+    .connect("mongodb+srv://FarhangizHabibi:12344321@cluster1.y9abtdr.mongodb.net/t" )
     .then(() => {
       console.log("Connected to database!");
     })
